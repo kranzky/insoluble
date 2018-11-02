8349f76f894a2a82c87ffb290f66da22639afd90
Jason Hutchens <jasonhutchens@gmail.com>
2018-11-02 23:02:17 +0800

    WIP; playing around with template.txt

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
+#include "dictionary.h"
+#include "tokenizer.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 
 //==============================================================================
+
+// process arguments
+// read from utf-8 file and write to utf-8 file
+// support multiple models
 
 int main(int argc, char **argv)
 {
