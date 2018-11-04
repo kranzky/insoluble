9a27ecace25b8ac245c1c6fd596f99d56266bcff
Jason Hutchens <jasonhutchens@gmail.com>
2018-11-04 15:37:29 +0800

    sentence segmentation

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ ruby '2.5.3'
 
 gem 'awesome_print', '~> 1.8'
 gem 'byebug', '~> 10.0'
+gem 'pragmatic_segmenter', '~> 0.3'
--- script/associate.rb
+++ script/associate.rb
@@ -2,39 +2,81 @@
 
 require "awesome_print"
 require 'byebug'
+require 'pragmatic_segmenter'
 
-# iterate through all gutenberg texts
+def _each_sentence(lines)
+  lines.map!(&:strip!)
+  blob = lines.join(' ')
+  ps = PragmaticSegmenter::Segmenter.new(text: blob)
+  ps.segment.each do |line|
+    # remove text within square brackets from line
+    # ignore if all punctuation or empty
+    # tag as dialogue or exposition
+    yield line
+  end
+end
 
-# for a particular text, search for a chapter marker
-  # - begins with uppercase CHAPTER or uppercase roman numeral
-  # - followed by at least 10 lines (so not in TOC)
+def _each_paragraph(lines)
+  paragraph = []
+  in_paragraph = false
+  lines.each do |line|
+    if line.strip.empty?
+      yield paragraph if paragraph.length > 0
+      paragraph = []
+    else
+      paragraph << line
+    end
+  end
+end
 
 def _each_chapter(lines)
   chapter = []
-  inside = false
+  preamble = []
+  in_chapter = false
+  in_preamble = false
   lines.each do |line|
-    if line =~ /^CHAPTER\s/
-      yield chapter if chapter.length > 9
+    if line =~ /^CHAPTER\s/ || line =~ /^[IVX]+(\s\.-)/ || line.strip =~ /^[IXV]+$/ || line =~ /^_Chapter/ || line =~ /Chapter/
+      yield chapter if chapter.length > 49
       chapter = []
-      inside = true
-    elsif inside
-      chapter << line
+      preamble = []
+      in_chapter = true
+      in_preamble = true
+    elsif line =~ /^PUBLICATIONS/ || line.strip =~ /^THE END/ || line.strip =~ /^(APPENDIX|Appendix|TAGGARD|Footnotes|PRINTED|FOOTNOTES|Henry James\'s Books)/
+      yield chapter if chapter.length > 49
+      chapter = []
+      preamble = []
+      in_chapter = false
+      in_preamble = false
+    elsif in_chapter
+      if !line.strip.empty? && line !~ /^\s/ && line.strip =~ /[a-z]/ && line !~ /[\[]/
+        in_preamble = false
+      end
+      chapter << line unless in_preamble
+      preamble << line if in_preamble
+      if line =~ /^\s+[^\s]/ && !in_preamble
+        chapter = []
+        preamble = []
+        in_chapter = false
+        in_preamble = false
+      end
     end
   end
-  yield chapter if chapter.length > 9
+  yield chapter if chapter.length > 49
 end
 
 def _process(filename)
   lines = File.readlines(filename)
   count = 0
   _each_chapter(lines) do |chapter|
-    puts "===#{filename}===" if count == 0
-    count += 1
-    puts "---#{count}---"
-    puts chapter.join
+    _each_paragraph(chapter) do |paragraph|
+      _each_sentence(paragraph) do |sentence|
+        # puts sentence
+      end
+      puts "==="
+    end
   end
 end
 
-Dir.glob('gutenberg/*.txt').each do |filename|
+Dir.glob('gutenberg/*.txt').shuffle.each do |filename|
   _process(filename)
 end
