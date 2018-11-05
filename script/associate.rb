52c58e1cac57d274f723f5a384431a8c8148dad2
Jason Hutchens <jasonhutchens@gmail.com>
2018-11-05 22:45:20 +0800

    wip of using npmi to generate keywords

--- script/associate.rb
+++ script/associate.rb
@@ -11,6 +11,8 @@ def _each_sentence(lines)
   blob = lines.join(' ')
   blob.gsub!(/\s*\[([^\[\]]+)\]\s*/, ' ')
   blob.gsub!(/\s*\[([^\[\]]+)\]\s*/, ' ')
+  blob.gsub!(/[_]/, '')
+  blob.gsub!(/\.\.\.+/, '…')
   ps = PragmaticSegmenter::Segmenter.new(text: blob)
   ps.segment.each do |line|
     next if line.strip.empty?
@@ -69,15 +71,16 @@ def _each_chapter(lines)
   yield chapter if chapter.length > 49
 end
 
-def _learn(predictor, prev_sentence, sentence)
+def _learn(predictor, prev_sentence, sentence, skip=false)
   prev_sentence.sort.uniq.each do |event|
     sentence.sort.uniq.each do |action|
+      next if skip && action == event
       predictor.observe(event, action)
     end
   end
 end
 
-def _process(filename, next_predictor, prev_predictor, dictionary)
+def _process(filename, next_predictor, prev_predictor, scan_predictor, dictionary)
   count = 0
   lines = File.readlines(filename)
   _each_chapter(lines) do |chapter|
@@ -89,10 +92,12 @@ def _process(filename, next_predictor, prev_predictor, dictionary)
         sentence = sentence.first.map { |word| dictionary[word] ||= dictionary.length }
         _learn(next_predictor, prev_sentence, sentence)
         _learn(prev_predictor, sentence, prev_sentence)
+        _learn(scan_predictor, sentence, sentence, true)
         prev_sentence = sentence
       end
       _learn(next_predictor, prev_sentence, [1])
       _learn(prev_predictor, [1], prev_sentence)
+      _learn(scan_predictor, prev_sentence, prev_sentence, true)
     end
   end
   count
@@ -120,29 +125,70 @@ def _decompose(line, maximum_length=1024)
 	[puncs, norms, words]
 end
 
+def _infomagnetism(pair_predictor, predictor, event, action, total)
+  p_action = predictor.count(action) / total.to_f
+  p_event = predictor.count(event) / total.to_f
+  p_action_given_event = pair_predictor.frequency(event, action)
+  if p_action == 0 || p_event == 0 || p_action_given_event == 0
+    return 0
+  end
+  p_action_given_event * p_event * (Math.log2(p_action_given_event) - Math.log2(p_action))
+end
+
+def _keywords(sentences, next_predictor, prev_predictor, scan_predictor, dictionary, decode, total)
+  return unless sentences.length == 3
+  results = Hash.new { |h, k| h[k] = 0 }
+  dictionary.values.each do |id|
+    next if id < 2
+    sentences[0].last.each do |word|
+      force = _infomagnetism(next_predictor, scan_predictor, word, id, total)
+      results[id] += force if force > results[id]
+    end
+    sentences[1].last.each do |word|
+      next if id == word
+      force = _infomagnetism(scan_predictor, scan_predictor, word, id, total)
+      results[id] += force if force > results[id]
+    end
+    sentences[2].last.each do |word|
+      force = _infomagnetism(prev_predictor, scan_predictor, word, id, total)
+      results[id] += force if force > results[id]
+    end
+  end
+  keywords = []
+  candidates = results.to_a.sort { |a, b| b[1] <=> a[1] }[0..2]
+  candidates.each do |v|
+    keywords << v[0] if v[1] > 0
+  end
+  [[sentences[1].first,sentences[1].last.length], keywords.shuffle.map { |w| decode[w] }]
+end
+
 dictionary = { "<error>" => 0, "<blank>" => 1 }
 next_predictor = Sooth::Predictor.new(0)
 prev_predictor = Sooth::Predictor.new(0)
+scan_predictor = Sooth::Predictor.new(0)
 files = Dir.glob('gutenberg/*.txt').shuffle
-files = files[0..9]
+files = files[0..499]
 bar = ProgressBar.create(total: files.count)
 count = 0
 files.each do |filename|
-  count += _process(filename, next_predictor, prev_predictor, dictionary)
+  count += _process(filename, next_predictor, prev_predictor, scan_predictor, dictionary)
   bar.increment
 end
-
-# we have {count} sentences
-# we have a model that counts event in first sentence and action in second sentence
-# for a particular sentence, can find all actions, and the NPMI of each action
-# either predictor can be used to find rare words in a sentence
 
 decode = Hash[dictionary.to_a.map(&:reverse)]
 
 lines = File.readlines("template.txt")
+sentences = []
+sentences << [:control, [1]]
 lines.each do |line|
   line.strip!
   if ['CHAPTER','PARAGRAPH'].include?(line) 
+    sentences << [:control, [1]]
+    sentences.shift while sentences.length > 3
+    keywords = _keywords(sentences, next_predictor, prev_predictor, scan_predictor, dictionary, decode, count)
+    puts "#{keywords.first.join(';')}:#{keywords.last.join(' ')}" unless keywords.nil?
+    sentences = []
+    sentences << [:control, [1]]
     puts line
     next
   end
@@ -150,6 +196,12 @@ lines.each do |line|
   puncs, norms, words = _decompose(line)
   next if norms.nil? || norms.empty?
   sentence = norms.map { |word| dictionary[word] }.compact.sort.uniq
-  sentence.map! { |id| decode[id] }
-  puts "#{type}:#{sentence.join(' ')}"
+  sentences << [type, sentence]
+  sentences.shift while sentences.length > 3
+  keywords = _keywords(sentences, next_predictor, prev_predictor, scan_predictor, dictionary, decode, count)
+  puts "#{keywords.first.join(';')}:#{keywords.last.join(' ')}" unless keywords.nil?
 end
+sentences << [:control, [1]]
+sentences.shift while sentences.length > 3
+keywords = _keywords(sentences, next_predictor, prev_predictor, scan_predictor, dictionary, decode, count)
+puts "#{keywords.first.join(';')}:#{keywords.last.join(' ')}" unless keywords.nil?
